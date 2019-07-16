3dbeec842a7a45b7674f5398b1c4b3cc5534eb72
shjo <shjo@MZC01-SHJO>
2019-07-16 19:38:13 +0900

    commit

--- src/main/java/me/devstar/web/devStarMallVue/DevStarMallVueApplilcation.java
+++ src/main/java/me/devstar/web/devStarMallVue/DevStarMallVueApplilcation.java
@@ -18,5 +18,17 @@ public class DevStarMallVueApplilcation {
 	public String index(Model model, final SearchForm searchForm) {
 		return "index";
 	}
+	
+	@GetMapping("/vuetest01")
+	public String vuetest01(Model model, final SearchForm searchForm) {
+		return "vuetest01";
+	}
+	
+	
+	@GetMapping("/vuetest02")
+	public String vuetest02(Model model, final SearchForm searchForm) {
+		return "vuetest02";
+	}
+
 
 }
